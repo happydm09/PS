a705f073e652a53a5d95209959fc3091181f7c99
DM <100419204+happydm09@users.noreply.github.com>
2023-06-27 20:09:50 +0900

    Create 2022-R1-T1.py

---
+++ NYPC/2022-R1-T1.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+ci = 0
+
+for _ in range(M):
+    t, i, s = map(int, input().split())
+    if t < 60:
+        print('NO')
+        break
+    if i == ci:
+        print('NO')
+        break
+    ci = i
+else:
+    print('YES')
